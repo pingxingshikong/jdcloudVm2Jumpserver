562013fe33bfc067d9cc10be8f933545d7df5856
zhanenqiang <1604030622@qq.com>
2024-07-18 09:25:59 +0800

    chore:add tag prefix

--- src/jdcloudVm2Jumpserver/init.go
+++ src/jdcloudVm2Jumpserver/init.go
@@ -1,12 +1,8 @@
 package jdcloudVm2Jumpserver
 
 import (
-	"github.com/jdcloud-api/jdcloud-sdk-go/services/vm/models"
 	"jdcloudVm2Jumpserver/src/jdcloudVm2Jumpserver/config"
-	"jdcloudVm2Jumpserver/src/jdcloudVm2Jumpserver/jdcloud"
-	"jdcloudVm2Jumpserver/src/jdcloudVm2Jumpserver/jumpserver"
 	"log"
-	"strings"
 	"time"
 )
 
@@ -33,54 +29,4 @@ func Init() {
 		// 等待下一个定时器触发
 		<-ticker.C
 	}
-}
-
-func runTask(config *config.Config) {
-
-	// 获取 Jumpserver token
-	token, err := jumpserver.GetToken(config.Jumpserver.URL, config.Jumpserver.User, config.Jumpserver.Password)
-	if err != nil {
-		log.Printf("Error getting Jumpserver token: %v", err)
-		return
-	}
-
-	// 获取资产列表
-	assetMap, err := jumpserver.FetchAssetObjectListLabels(config, config.Jumpserver.URL, "/api/v1/assets/hosts/", token)
-	if err != nil {
-		log.Fatalf("Error fetching asset list: %v", err)
-		return
-	}
-	log.Println("Fetched asset list: ", assetMap)
-	// 遍历所有地域，获取云主机列表
-	var allInstances []models.Instance
-
-	for _, region := range config.JDCloud.Regions {
-		instances, err := jdcloud.GetCloudHostList(config.JDCloud.AccessKey, config.JDCloud.SecretKey, region)
-		if err != nil {
-			log.Printf("Error getting cloud host list for region %s: %v \n", region, err)
-			continue
-		}
-		matchedInstances := matchJdCloudVmTag(instances, config.Tags)
-		allInstances = append(allInstances, matchedInstances...)
-	}
-
-	// 打印云主机数量
-	log.Printf("Total number of cloud hosts: %d\n", len(allInstances))
-
-	var cloudHostAddresses []string
-	for _, host := range allInstances {
-		cloudHostAddresses = append(cloudHostAddresses, host.PrivateIpAddress)
-	}
-	if cloudHostAddresses != nil && len(cloudHostAddresses) > 0 {
-		log.Printf("Fetched cloud host list: %s \n", strings.Join(cloudHostAddresses, ", "))
-	}
-
-	jumpserver.DeleteByJdCloudNotExistButJumpServerExist(config, token, allInstances)
-
-	jumpserver.DeleteJumpServerInstance(config, token, allInstances)
-
-	jumpserver.CreteJumpServerInstance(config, token, allInstances)
-
-	jumpserver.UpdateJumpServerInstance(config, token, allInstances)
-
 }
--- src/jdcloudVm2Jumpserver/jd2jumpserver.go
+++ src/jdcloudVm2Jumpserver/jd2jumpserver.go
@@ -4,7 +4,60 @@ import (
 	disk "github.com/jdcloud-api/jdcloud-sdk-go/services/disk/models"
 	"github.com/jdcloud-api/jdcloud-sdk-go/services/vm/models"
 	"jdcloudVm2Jumpserver/src/jdcloudVm2Jumpserver/config"
+	"jdcloudVm2Jumpserver/src/jdcloudVm2Jumpserver/jdcloud"
+	"jdcloudVm2Jumpserver/src/jdcloudVm2Jumpserver/jumpserver"
+	"log"
+	"strings"
 )
+
+func runTask(config *config.Config) {
+	// 获取 Jumpserver token
+	token, err := jumpserver.GetToken(config.Jumpserver.URL, config.Jumpserver.User, config.Jumpserver.Password)
+	if err != nil {
+		log.Printf("Error getting Jumpserver token: %v", err)
+		return
+	}
+
+	// 获取资产列表
+	assetMap, err := jumpserver.FetchAssetObjectListLabels(config, config.Jumpserver.URL, "/api/v1/assets/hosts/", token)
+	if err != nil {
+		log.Fatalf("Error fetching asset list: %v", err)
+		return
+	}
+	log.Println("Fetched asset list: ", assetMap)
+	// 遍历所有地域，获取云主机列表
+	var allInstances []models.Instance
+
+	for _, region := range config.JDCloud.Regions {
+		instances, err := jdcloud.GetCloudHostList(config.JDCloud.AccessKey, config.JDCloud.SecretKey, region)
+		if err != nil {
+			log.Printf("Error getting cloud host list for region %s: %v \n", region, err)
+			continue
+		}
+		matchedInstances := matchJdCloudVmTag(instances, config.Tags)
+		allInstances = append(allInstances, matchedInstances...)
+	}
+
+	// 打印云主机数量
+	log.Printf("Total number of cloud hosts: %d\n", len(allInstances))
+
+	var cloudHostAddresses []string
+	for _, host := range allInstances {
+		cloudHostAddresses = append(cloudHostAddresses, host.PrivateIpAddress)
+	}
+	if cloudHostAddresses != nil && len(cloudHostAddresses) > 0 {
+		log.Printf("Fetched cloud host list: %s \n", strings.Join(cloudHostAddresses, ", "))
+	}
+
+	jumpserver.DeleteByJdCloudNotExistButJumpServerExist(config, token, allInstances)
+
+	jumpserver.DeleteJumpServerInstance(config, token, allInstances)
+
+	jumpserver.CreteJumpServerInstance(config, token, allInstances)
+
+	jumpserver.UpdateJumpServerInstance(config, token, allInstances)
+
+}
 
 // matchTags 获取实例中匹配配置文件中标签键的所有标签值
 func matchTags(instanceTags []disk.Tag, configTags []config.Tag) []string {
